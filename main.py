407f01494723186e8db92f475502fe744beec31d
thanglegons <dthangnguyen19@gmail.com>
2021-04-25 17:10:04 +0700

    add main 2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi khanh, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi khanh <3, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
